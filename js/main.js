b0e2e81c88b1202914ac6fad08b8f6d627fbe367
Surya Pratap Singh <suryaprataps424@gmail.com>
2023-11-07 20:00:51 +0530

    first commit

--- js/main.js
+++ js/main.js
@@ -5,8 +5,9 @@ function boxesAudioPlayer1() {
     let playAudioMix1 = document.querySelector("#audio-play-mix-1");
     let afterMusic1 = document.querySelector("#boxe-1 #after-btn-1");
     let beforeMusic1 = document.querySelector("#boxe-1 #before-btn-1");
-    let progreesThumb = document.querySelector("#boxe-1 .progree-player__thumb");
-    let progressCount = document.querySelector("#boxe-1 .progree-player__count");
+    let progreesThumb1 = document.querySelector("#boxe-1 .progree-player__thumb");
+    let progressCount1 = document.querySelector("#boxe-1 .progree-player__count");
+    // const progressPlayers = document.querySelectorAll(".progree-player");
 
     playBtn1.addEventListener("click", () => {
         if (playAudio1.paused) {
@@ -37,6 +38,7 @@ function boxesAudioPlayer1() {
     beforeMusic1.addEventListener("click", () => {
         if (playAudio1.paused) {
             playAudio1.play();
+            playAudioMix1.pause();
             playBtn1.style.display = "none"; // Hide the play button
             pauseBtn1.style.display = "block"; // Show the pause button
         }
@@ -45,20 +47,48 @@ function boxesAudioPlayer1() {
     afterMusic1.addEventListener("click", () => {
         if (playAudioMix1.paused) {
             playAudioMix1.play();
+            playAudio1.pause();
             playBtn1.style.display = "none"; // Hide the play button
             pauseBtn1.style.display = "block"; // Show the pause button
         }
     });
+
+    pauseBtn1.addEventListener("click", () => {
+        if (!playAudioMix1.paused) {
+            playAudioMix1.pause();
+            pauseBtn1.style.display = "none"; // Hide the pause button
+            playBtn1.style.display = "block"; // Show the play button
+        }
+    });
+
+
+
+
     playAudio1.addEventListener("timeupdate", () => {
         const currentTime = playAudio1.currentTime;
         const duration = playAudio1.duration;
         const progress = (currentTime / duration) * 100;
         console.log(progress);
         // Update the width of the progress bar's thumb
-        progreesThumb.style.width = `${progress}%`;
-        progressCount.style.right = `calc(${progress}% - ${progressCount.clientWidth / 2}px)`;
-        
+        progreesThumb1.style.left = `${progress}%`;
+        progressCount1.style.width = `${progress}%`;
+
     });
+
+
+
+    playAudioMix1.addEventListener("timeupdate", () => {
+        const currentTime = playAudioMix1.currentTime;
+        const duration = playAudioMix1.duration;
+        const progress = (currentTime / duration) * 100;
+        console.log(progress);
+        // Update the width of the progress bar's thumb
+        progreesThumb1.style.left = `${progress}%`;
+        progressCount1.style.width = `${progress}%`;
+
+    });
+
+
 }
 
 boxesAudioPlayer1();
@@ -75,6 +105,9 @@ function boxesAudioPlayer2() {
     let playAudioMix2 = document.querySelector("#audio-play-mix-2");
     let afterMusic2 = document.querySelector("#boxe-2 #after-btn-2");
     let beforeMusic2 = document.querySelector("#boxe-2 #before-btn-2");
+    let progreesThumb2 = document.querySelector("#boxe-2 .progree-player__thumb");
+    let progressCount2 = document.querySelector("#boxe-2 .progree-player__count");
+    // const progressPlayers2 = document.querySelectorAll(".progree-player");
 
     playBtn2.addEventListener("click", () => {
         if (playAudio2.paused) {
@@ -105,6 +138,7 @@ function boxesAudioPlayer2() {
     beforeMusic2.addEventListener("click", () => {
         if (playAudio2.paused) {
             playAudio2.play();
+            playAudioMix2.pause();
             playBtn2.style.display = "none"; // Hide the play button
             pauseBtn2.style.display = "block"; // Show the pause button
         }
@@ -113,9 +147,42 @@ function boxesAudioPlayer2() {
     afterMusic2.addEventListener("click", () => {
         if (playAudioMix2.paused) {
             playAudioMix2.play();
+            playAudio2.pause();
             playBtn2.style.display = "none"; // Hide the play button
             pauseBtn2.style.display = "block"; // Show the pause button
         }
+    });
+    
+    pauseBtn2.addEventListener("click", () => {
+        if (!playAudioMix2.paused) {
+            playAudioMix2.pause();
+            pauseBtn2.style.display = "none"; // Hide the pause button
+            playBtn2.style.display = "block"; // Show the play button
+        }
+    });
+
+    playAudio2.addEventListener("timeupdate", () => {
+        const currentTime = playAudio2.currentTime;
+        const duration = playAudio2.duration;
+        const progress2 = (currentTime / duration) * 100;
+        console.log(progress2);
+        // Update the width of the progress bar's thumb
+        progreesThumb2.style.left = `${progress2}%`;
+        progressCount2.style.width = `${progress2}%`;
+
+    });
+    
+
+
+    playAudioMix2.addEventListener("timeupdate", () => {
+        const currentTime = playAudioMix2.currentTime;
+        const duration = playAudioMix2.duration;
+        const progress2 = (currentTime / duration) * 100;
+        console.log(progress2);
+        // Update the width of the progress bar's thumb
+        progreesThumb2.style.left = `${progress2}%`;
+        progressCount2.style.width = `${progress2}%`;
+
     });
 }
 
@@ -131,6 +198,9 @@ function boxesAudioPlayer3() {
     let playAudioMix3 = document.querySelector("#audio-play-mix-3");
     let afterMusic3 = document.querySelector("#boxe-3 #after-btn-3");
     let beforeMusic3 = document.querySelector("#boxe-3 #before-btn-3");
+    let progreesThumb3 = document.querySelector("#boxe-3 .progree-player__thumb");
+    let progressCount3 = document.querySelector("#boxe-3 .progree-player__count");
+    // const progressPlayers3 = document.querySelectorAll(".progree-player");
 
 
     playBtn3.addEventListener("click", () => {
@@ -163,6 +233,7 @@ function boxesAudioPlayer3() {
     beforeMusic3.addEventListener("click", () => {
         if (playAudio3.paused) {
             playAudio3.play();
+            playAudioMix3.pause();
             playBtn3.style.display = "none"; // Hide the play button
             pauseBtn3.style.display = "block"; // Show the pause button
         }
@@ -171,9 +242,41 @@ function boxesAudioPlayer3() {
     afterMusic3.addEventListener("click", () => {
         if (playAudioMix3.paused) {
             playAudioMix3.play();
+            playAudio3.pause();
             playBtn3.style.display = "none"; // Hide the play button
             pauseBtn3.style.display = "block"; // Show the pause button
         }
+    });
+    
+    pauseBtn3.addEventListener("click", () => {
+        if (!playAudioMix3.paused) {
+            playAudioMix3.pause();
+            pauseBtn3.style.display = "none"; // Hide the pause button
+            playBtn3.style.display = "block"; // Show the play button
+        }
+    });
+
+    playAudio3.addEventListener("timeupdate", () => {
+        const currentTime = playAudio3.currentTime;
+        const duration = playAudio3.duration;
+        const progress3 = (currentTime / duration) * 100;
+        console.log(progress3);
+        // Update the width of the progress bar's thumb
+        progreesThumb3.style.left = `${progress3}%`;
+        progressCount3.style.width = `${progress3}%`;
+
+    });
+
+
+    playAudioMix3.addEventListener("timeupdate", () => {
+        const currentTime = playAudioMix3.currentTime;
+        const duration = playAudioMix3.duration;
+        const progress3 = (currentTime / duration) * 100;
+        console.log(progress3);
+        // Update the width of the progress bar's thumb
+        progreesThumb3.style.left = `${progress3}%`;
+        progressCount3.style.width = `${progress3}%`;
+
     });
 }
 
--- js/new.js
+++ js/new.js
@@ -1,51 +1,79 @@
 class AudioPlayer {
-    constructor(container) {
-        this.container = container;
-        this.playBtn = container.querySelector(".music-play");
-        this.playAudio = container.querySelector("audio");
-        this.pauseBtn = container.querySelector(".music-off");
-        this.playAudioMix = container.querySelector("audio:nth-of-type(2)");
-        this.progressThumb = container.querySelector(".progree-player__thumb");
-        this.progressCount = container.querySelector(".progree-player__count");
+  constructor(container) {
+    this.container = container;
+    this.playBtn = container.querySelector(".music-play");
+    this.playAudio = container.querySelector("audio");
+    this.pauseBtn = container.querySelector(".music-off");
+    this.playAudioMix = container.querySelector("audio:nth-of-type(2)");
+    this.progressThumb = container.querySelector(".progree-player__thumb");
+    this.progressCount = container.querySelector(".progree-player__count");
+    this.beforeMusic = container.querySelector(".before-btn");
+    this.afterMusic = container.querySelector(".after-btn");
+    this.init();
+  }
 
-        this.init();
-    }
+  init() {
+    this.playBtn.addEventListener("click", () => this.playAudioTrack());
+    this.pauseBtn.addEventListener("click", () => this.pauseAudioTrack());
+    this.playAudio.addEventListener("timeupdate", () => this.updateProgress());
+    this.beforeMusic.addEventListener("click", () => this.playAudioTrack());
+    this.afterMusic.addEventListener("click", () => this.playAudioMixTrack());
+  }
+  playAudioTrack() {
+    this.playAudio.play();
+    this.playAudioMix.pause();
+    this.updatePlayPauseButtons();
+  }
+  playAudioMixTrack() {
+    this.playAudioMix.play();
+    this.playAudio.pause();
+    this.updatePlayPauseButtons();
+  }
+  pauseAudioTrack() {
+    this.playAudio.pause();
+    this.playAudioMix.pause();
+    this.updatePlayPauseButtons();
+  }
+  stopAudioTrack() {
+    this.playAudio.pause();
+    this.playAudio.currentTime = 0;
+    this.playAudioMix.pause();
+    this.playAudioMix.currentTime = 0;
+    this.updatePlayPauseButtons();
+  }
 
-    init() {
-        this.playBtn.addEventListener("click", () => this.togglePlayback());
-        this.pauseBtn.addEventListener("click", () => this.togglePlayback());
-        this.playAudio.addEventListener("timeupdate", () => this.updateProgress());
+  updatePlayPauseButtons() {
+    if (this.playAudio.paused && this.playAudioMix.paused) {
+      this.playBtn.style.display = "block";
+      this.pauseBtn.style.display = "none";
+    } else {
+      this.playBtn.style.display = "none";
+      this.pauseBtn.style.display = "block";
     }
+  }
 
-    togglePlayback() {
-        if (this.playAudio.paused) {
-            this.playAudio.play();
-            this.playBtn.style.display = "none";
-            this.pauseBtn.style.display = "block";
-        } else {
-            this.playAudio.pause();
-            this.pauseBtn.style.display = "none";
-            this.playBtn.style.display = "block";
-        }
-    }
+  updateProgress() {
+    const currentTime = this.playAudio.currentTime;
+    const duration = this.playAudio.duration;
+    const progress = (currentTime / duration) * 100;
+    this.progressThumb.style.width = `${progress}%`;
+    this.progressCount.style.left = `calc(${progress}% - ${
+      this.progressCount.clientWidth / 2
+    }px)`;
+    this.progressCount.textContent = this.formatTime(currentTime);
+  }
 
-    updateProgress() {
-        const currentTime = this.playAudio.currentTime;
-        const duration = this.playAudio.duration;
-        const progress = (currentTime / duration) * 100;
-        this.progressThumb.style.width = `${progress}%`;
-        this.progressCount.style.left = `calc(${progress}% - ${this.progressCount.clientWidth / 2}px)`;
-        this.progressCount.textContent = this.formatTime(currentTime);
-    }
-
-    formatTime(time) {
-        const minutes = Math.floor(time / 60);
-        const seconds = Math.floor(time % 60);
-        return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-    }
+  formatTime(time) {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    return `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(
+      2,
+      "0"
+    )}`;
+  }
 }
 
 // Create instances for each player box
 const player1 = new AudioPlayer(document.querySelector(".box:nth-of-type(1)"));
-const player2 = new AudioPlayer(document.querySelector(".box:nth-of-type(2)"));
-const player3 = new AudioPlayer(document.querySelector(".box:nth-of-type(3)"));
+// const player2 = new AudioPlayer(document.querySelector(".box:nth-of-type(2)"));
+// const player3 = new AudioPlayer(document.querySelector(".box:nth-of-type(3)"));
